2a4ec120c51c06007cc1bd92e35d6560ff149bf2
BrunoRodrigues11 <65250099+BrunoRodrigues11@users.noreply.github.com>
2021-03-20 17:18:21 -0300

    Add files via upload

---
+++ gameTV/html/menu.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Área Administrativa</title>
+    <!-- CÓDIGPO DESABILITADO PARA NÃO HAVER CONFLITOS
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
+</head>
+<body>
+    <!-- Inicio navbar -->
+  <nav class="nav navbar-fixed-top  navbar-inverse menu">
+    <div class="container-fluid">
+      <!-- Agrupamento para exibição MOBILE -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#defaultNavbar"
+          aria-expanded="false"">
+        <span class=" sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="">
+          <img src="img/Logo.svg" alt="">
+        </a>
+      </div>
+      <!-- Nav direita -->
+      <div class="collapse navbar-collapse" id="defaultNavbar">
+        <ul class="nav navbar-nav">
+          <li class=""><a href="index.html">HOME</a></li>
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">JOGOS
+              <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">GTA V</a></li>
+              <li><a href="#">Cities Skylines</a></li>
+              <li><a href="#">PES</a></li>
+            </ul>
+          </li>
+          <li><a href="#contato">CONTATO</a></li>
+        </ul>
+        <!-- Nav direita -->
+        <div class="espacoDireita">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"> Carrinho</span></a></li>
+            <li><a href="html/login.html"><span class="glyphicon glyphicon-log-in"></span> Entrar</a></li>
+          </ul>
+        </div>
+      </div>
+    </div><!-- fechamento container-fluid -->
+  </nav>
+
+<!-- CÓDIGPO DESABILITADO PARA NÃO HAVER CONFLITOS
+ Link arquivos bootstrap js 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="../bootstrap/js/bootstrap.min.js"></script> -->
+</body>
+</html>
